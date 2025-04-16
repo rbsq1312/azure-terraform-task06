a882bbc7296e5a51f997771da878432f05e41422
Robescu Theodor <rbscthdr@protonmail.com>
2025-04-16 00:14:46 +0000

    put sensitive above value

--- task06/modules/sql/outputs.tf
+++ task06/modules/sql/outputs.tf
@@ -17,6 +17,6 @@ output "sql_database_id" {
 
 output "sql_connection_string" {
   description = "The ADO.NET connection string for the SQL database"
-  value       = "Server=tcp:${azurerm_mssql_server.sql_server.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.sql_db.name};Persist Security Info=False;User ID=${var.sql_admin_username};Password=${random_password.sql_admin_password.result};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
   sensitive   = true
+  value       = "Server=tcp:${azurerm_mssql_server.sql_server.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.sql_db.name};Persist Security Info=False;User ID=${var.sql_admin_username};Password=${random_password.sql_admin_password.result};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
 }
